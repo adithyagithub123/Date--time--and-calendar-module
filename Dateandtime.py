6d8824ba1c1a477b9850d83d6b0f0ed5b779405e
adithyagithub123 <novox1gamer@gmail.com>
2024-10-24 19:46:53 +0400

    Date, time, and calendar module

---
+++ Dateandtime.py
@@ -0,0 +1,9 @@
+from datetime import date , time , datetime
+today = date.today()
+now = datetime.now()
+
+print("the date today is " , today )
+print("The time now is " , now)
+
+print(today.year , "/" , today.month , "/" , today.day)
+
